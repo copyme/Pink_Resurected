f9a172e478a60663df5fe74baf5063d21b841eb3
copyme <kacper.pluta@gmail.com>
2025-11-25 09:29:19 +0100

    some improvements to signatures

--- src/include/jcimage.h
+++ src/include/jcimage.h
@@ -47,16 +47,16 @@ extern "C" {
 // Les deux deux fichiers doivent etre utiliss conjointement
 
 extern struct xvimage4D *allocimage4D(int32_t ss);
-extern struct xvimage *allocGAimage(char * name, int32_t rs, int32_t cs, int32_t d, int32_t t);
-extern void writerawGAimage(struct xvimage * image, char *filename);
-extern struct xvimage * readGAimage(char *filename);
-extern struct xvimage4D *readimage4D(char *prefix, int32_t first, int32_t last);     
+extern struct xvimage *allocGAimage(const char * name, int32_t rs, int32_t cs, int32_t d, int32_t t);
+extern void writerawGAimage(const struct xvimage * image, char *filename);
+extern struct xvimage * readGAimage(const char *filename);
+extern struct xvimage4D *readimage4D(const char *prefix, int32_t first, int32_t last);
 extern void freeimage4D(struct xvimage4D  *im);
-extern struct GA4d * readGA4d(char *filename);
-extern struct GA4d * allocGA4d(char * name, int32_t rs, int32_t cs, int32_t d, int32_t ss);
-extern void writeGA4d(struct GA4d * image, char *filename);
+extern struct GA4d * readGA4d(const char *filename);
+extern struct GA4d * allocGA4d(const char * name, int32_t rs, int32_t cs, int32_t d, int32_t ss);
+extern void writeGA4d(const struct GA4d * image, const char *filename);
 extern void freeGA4d(struct GA4d  *im);
-void writeimage4D(struct xvimage4D * image, char *prefix, int32_t first, int32_t last);
+void writeimage4D(const struct xvimage4D* image, const char* prefix, int32_t first);
 #ifdef __cplusplus
 }
 #endif
